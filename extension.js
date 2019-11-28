1f38b5f81113eff39b2785c9a25b3448e6ce591c
Raphael Balet <r.balet@ultrastark.ch>
2019-11-28 13:31:58 +0100

    enhance - extension :  add [ / and *

--- extension.js
+++ extension.js
@@ -1,92 +1,117 @@
-'use strict';
+'use strict'
 
-const vscode = require('vscode');
-const htmlSorter = require('./lib/html');
-const jadeSorter = require('./lib/jade');
+const vscode = require('vscode')
+const htmlSorter = require('./lib/html')
+const jadeSorter = require('./lib/jade')
 
-let output;
+let output
 
 function sort(document, range) {
-	const options = Object.assign({}, vscode.workspace.getConfiguration('attrsSorter'));
+	const options = Object.assign({}, vscode.workspace.getConfiguration('attrsSorter'))
+	/** Following :
+	 *   https://codeguide.co/#html-lang
+	 *   https://stackoverflow.com/a/51389977/11135174
+	 *   Added Angular after data. See https://github.com/mdo/code-guide/issues/106
+	 */
+
+	// prettier-ignore
 	if (options.order.length === 0) {
-		options.order = null;
+		options.order = [
+      'id', 'class', 'name',
+      'data-.+', 'ng-.+', '\\[.+', '\\(.+', '\\*ng.+',
+      'src', 'for', 'type', 'href', 'value',
+      'title', 'alt',
+      'role', 'aria-.+',
+      '$unknown$'
+    ]
 	}
 
-	let text;
+	let text
 	if (range) {
-		text = document.getText(range);
+		text = document.getText(range)
 	} else {
-		const lastLine = document.lineAt(document.lineCount - 1);
-		const start = new vscode.Position(0, 0);
-		const end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+		const lastLine = document.lineAt(document.lineCount - 1)
+		const start = new vscode.Position(0, 0)
+		const end = new vscode.Position(document.lineCount - 1, lastLine.text.length)
 
-		range = new vscode.Range(start, end);
-		text = document.getText();
+		range = new vscode.Range(start, end)
+		text = document.getText()
 	}
 
 	// Jade
-	const lang = document.languageId;
+	const lang = document.languageId
 	if (lang === 'jade') {
 		try {
 			return Promise.resolve({
 				text: jadeSorter(text, options),
-				range
-			});
+				range,
+			})
 		} catch (err) {
-			return Promise.reject(err);
+			return Promise.reject(err)
 		}
 	}
 
 	// HTML
 	return htmlSorter(text, options).then((result) => ({
 		text: result.html,
-		range
-	}));
+		range,
+	}))
 }
 
 /**
  * Show message in output channel.
  */
 function showOutput(msg) {
-	msg = msg.toString();
+	msg = msg.toString()
 
 	if (!output) {
-		output = vscode.window.createOutputChannel('attrs-sorter');
+		output = vscode.window.createOutputChannel('attrs-sorter')
 	}
 
-	output.clear();
-	output.appendLine('[attrs-sorter]');
-	output.append(msg);
-	output.show();
+	output.clear()
+	output.appendLine('[attrs-sorter]')
+	output.append(msg)
+	output.show()
 }
 
 function activate(context) {
 	const supportedDocuments = [
 		{ language: 'html', scheme: 'file' },
-		{ language: 'jade', scheme: 'file' }
-	];
+		{ language: 'jade', scheme: 'file' },
+	]
 
-	const command = vscode.commands.registerTextEditorCommand('attrsSorter.execute', (textEditor) => {
-		sort(textEditor.document, null)
-			.then((result) => {
-				textEditor.edit((editBuilder) => {
-					editBuilder.replace(result.range, result.text);
-				});
-			})
-			.catch(showOutput);
-	});
+	const command = vscode.commands.registerTextEditorCommand(
+		'attrsSorter.execute',
+		(textEditor) => {
+			sort(textEditor.document, null)
+				.then((result) => {
+					textEditor.edit((editBuilder) => {
+						editBuilder.replace(result.range, result.text)
+					})
+				})
+				.catch(showOutput)
+		},
+	)
 
-	const formatCode = vscode.languages.registerDocumentRangeFormattingEditProvider(supportedDocuments, {
-		provideDocumentRangeFormattingEdits(document, range) {
-			return sort(document, range).then((result) => {
-				return [vscode.TextEdit.replace(range, result.text)];
-			}).catch(showOutput);
-		}
-	});
+	const formatCode = vscode.languages.registerDocumentRangeFormattingEditProvider(
+		supportedDocuments,
+		{
+			provideDocumentRangeFormattingEdits(document, range) {
+				return sort(document, range)
+					.then((result) => {
+						return [vscode.TextEdit.replace(range, result.text)]
+					})
+					.catch(showOutput)
+			},
+		},
+	)
 
 	// Subscriptions
-	context.subscriptions.push(command);
-	context.subscriptions.push(formatCode);
+	context.subscriptions.push(command)
+	context.subscriptions.push(formatCode)
 }
 
-exports.activate = activate;
+exports.activate = activate
+
+// this method is called when your extension is deactivated
+// export function deactivate() {}
